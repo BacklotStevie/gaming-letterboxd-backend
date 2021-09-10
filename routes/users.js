42c71e0ca635b850967b5f3bce9baea6712313d0
Steven Sarria <ssarr012@fiu.edu>
2021-09-10 16:41:06 -0400

    create signup route

--- routes/users.js
+++ routes/users.js
@@ -8,3 +8,40 @@ router.route('/').get((req, res) => {
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Errors: ' + err));
 })
+
+router.route('/signup').post((req, res) => {
+    //inputting bcrypt to hash passwords for security. The 10 is how many digits will hash.
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+        //make sure there is no other email in the database that matches the one being created
+        User.findOne({ email: req.body.email })
+            .then((user) => {
+                //if email is already found, user gets an error
+                if (user) return res.status(400).send("Email already exists");
+                else {
+                    //user is given a text field where they need to input all requirements to create a user
+                    return User.create({
+                        email: req.body.email,
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        //stores hash in our password DB
+                        password: hash,
+                        //once user is succesfully created they're assinged a special token
+                    }).then((user) => {
+                        let token = jwt.sign({ id: user._id }, 'shhhh')
+                        //once token is given, I would see the user email, token and usertype on my terminal and database
+                        res.status(200).json({
+                            token,
+                            user: {
+                                email: user.email,
+                                userType: user.userType
+                            }
+                        })
+                    })
+                }
+            })
+            //This occurs when the user has an error on their end.
+            .catch((err) => {
+                res.status(500).send('Whoops!')
+            })
+    });
+})
